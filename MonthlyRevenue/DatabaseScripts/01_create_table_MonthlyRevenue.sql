761bc3515e8ddff767e6d0415cf7ee490d34b11b
Danliu88090 <dan.liu@e-funtech.com>
2025-08-15 16:00:33 +0800

    SQL變更紀錄

---
+++ MonthlyRevenue/DatabaseScripts/01_create_table_MonthlyRevenue.sql
@@ -0,0 +1,45 @@
+﻿/*============================================================
+ 檔名：01_create_table_MonthlyRevenue.sql
+ 建立日期：2025-08-15
+ 修改紀錄：
+   - 2025-08-15 首版：建立資料表、主鍵、索引
+============================================================*/
+
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
+GO
+
+IF OBJECT_ID(N'dbo.MonthlyRevenue', N'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.MonthlyRevenue (
+        ReportDate               date            NULL,          -- 出表日期
+        DataYearMonth            char(6)         NOT NULL,      -- 資料年月 (YYYYMM)
+        CompanyCode              varchar(10)     NOT NULL,      -- 公司代號
+        CompanyName              nvarchar(120)   NOT NULL,      -- 公司名稱
+        Industry                 nvarchar(60)    NULL,          -- 產業別
+
+        Rev_CurrentMonth         bigint          NULL,          -- 當月營收
+        Rev_PreviousMonth        bigint          NULL,          -- 上月營收
+        Rev_SameMonthLastYear    bigint          NULL,          -- 去年同月營收
+        MoM_ChangePct            decimal(9,2)    NULL,          -- 月增率(%)
+        YoY_ChangePct            decimal(9,2)    NULL,          -- 年增率(%)
+
+        Rev_Accu_CurrentYear     bigint          NULL,          -- 本年累計營收
+        Rev_Accu_LastYear        bigint          NULL,          -- 上年累計營收
+        Accu_YoY_ChangePct       decimal(9,2)    NULL,          -- 累計年增率(%)
+
+        Notes                    nvarchar(400)   NULL,          -- 備註
+        CONSTRAINT PK_MonthlyRevenue PRIMARY KEY (DataYearMonth, CompanyCode)
+    );
+END
+GO
+
+IF NOT EXISTS (
+    SELECT 1 FROM sys.indexes
+    WHERE name = N'IX_MonthlyRevenue_Code_YM' AND object_id = OBJECT_ID(N'dbo.MonthlyRevenue')
+)
+BEGIN
+    CREATE INDEX IX_MonthlyRevenue_Code_YM
+        ON dbo.MonthlyRevenue (CompanyCode, DataYearMonth);
+END
+GO
---
+++ MonthlyRevenue/DatabaseScripts/02_proc_upsert_MonthlyRevenue.sql
@@ -0,0 +1,62 @@
+﻿/*============================================================
+ 檔名：02_proc_upsert_MonthlyRevenue.sql
+ 建立日期：2025-08-15
+ 修改紀錄：
+   - 2025-08-15 首版：Upsert（存在則更新，不在則新增）
+============================================================*/
+
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
+GO
+
+IF OBJECT_ID(N'dbo.usp_MonthlyRevenue_Upsert', N'P') IS NOT NULL
+    DROP PROC dbo.usp_MonthlyRevenue_Upsert;
+GO
+
+CREATE PROC dbo.usp_MonthlyRevenue_Upsert
+    @ReportDate            date,
+    @DataYearMonth         char(6),
+    @CompanyCode           varchar(10),
+    @CompanyName           nvarchar(120),
+    @Industry              nvarchar(60) = NULL,
+    @Rev_CurrentMonth      bigint = NULL,
+    @Rev_PreviousMonth     bigint = NULL,
+    @Rev_SameMonthLastYear bigint = NULL,
+    @MoM_ChangePct         decimal(9,2) = NULL,
+    @YoY_ChangePct         decimal(9,2) = NULL,
+    @Rev_Accu_CurrentYear  bigint = NULL,
+    @Rev_Accu_LastYear     bigint = NULL,
+    @Accu_YoY_ChangePct    decimal(9,2) = NULL,
+    @Notes                 nvarchar(400) = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    MERGE dbo.MonthlyRevenue AS T
+    USING (SELECT @DataYearMonth AS DataYearMonth, @CompanyCode AS CompanyCode) AS S
+    ON (T.DataYearMonth = S.DataYearMonth AND T.CompanyCode = S.CompanyCode)
+    WHEN MATCHED THEN
+        UPDATE SET
+            ReportDate            = @ReportDate,
+            CompanyName           = @CompanyName,
+            Industry              = @Industry,
+            Rev_CurrentMonth      = @Rev_CurrentMonth,
+            Rev_PreviousMonth     = @Rev_PreviousMonth,
+            Rev_SameMonthLastYear = @Rev_SameMonthLastYear,
+            MoM_ChangePct         = @MoM_ChangePct,
+            YoY_ChangePct         = @YoY_ChangePct,
+            Rev_Accu_CurrentYear  = @Rev_Accu_CurrentYear,
+            Rev_Accu_LastYear     = @Rev_Accu_LastYear,
+            Accu_YoY_ChangePct    = @Accu_YoY_ChangePct,
+            Notes                 = @Notes
+    WHEN NOT MATCHED THEN
+        INSERT (ReportDate, DataYearMonth, CompanyCode, CompanyName, Industry,
+                Rev_CurrentMonth, Rev_PreviousMonth, Rev_SameMonthLastYear,
+                MoM_ChangePct, YoY_ChangePct, Rev_Accu_CurrentYear, Rev_Accu_LastYear,
+                Accu_YoY_ChangePct, Notes)
+        VALUES (@ReportDate, @DataYearMonth, @CompanyCode, @CompanyName, @Industry,
+                @Rev_CurrentMonth, @Rev_PreviousMonth, @Rev_SameMonthLastYear,
+                @MoM_ChangePct, @YoY_ChangePct, @Rev_Accu_CurrentYear, @Rev_Accu_LastYear,
+                @Accu_YoY_ChangePct, @Notes);
+END
+GO
